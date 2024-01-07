e9452105a43821797bec01a7c426e1b1fda16b5d
uglyprincess <112811446+Uglypr1nces@users.noreply.github.com>
2024-01-07 17:54:23 +0100

    Add files via upload

---
+++ math1/MATH/MATH/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace MATH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Plack the Hanet", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MATH QUESTION";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(12, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 155);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.LightGreen;
+            this.button2.Location = new System.Drawing.Point(12, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(308, 155);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.button3.Location = new System.Drawing.Point(340, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(308, 155);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button4.Location = new System.Drawing.Point(340, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(308, 155);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Plack the Hanet", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "by uglyprincess";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Plack the Hanet", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(627, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(661, 449);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SlateBlue;
+            this.panel3.Controls.Add(this.button7);
+            this.panel3.Controls.Add(this.button6);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(330, 456);
+            this.panel3.TabIndex = 3;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightSlateGray;
+            this.button7.Font = new System.Drawing.Font("Monocraft", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(14, 350);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(303, 86);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "45";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightSlateGray;
+            this.button6.Font = new System.Drawing.Font("Monocraft", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(14, 256);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(303, 88);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "30";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightSlateGray;
+            this.button5.Font = new System.Drawing.Font("Monocraft", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(14, 166);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(303, 84);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "15";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(11, 7);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "by uglyprincess";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Plack the Hanet", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(243, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 29);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "WILK";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monocraft", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(249, 36);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "ANZAHL FRAGEN";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel2.Controls.Add(this.button9);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.button10);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(331, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(330, 459);
+            this.panel2.TabIndex = 2;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.SlateBlue;
+            this.button9.Font = new System.Drawing.Font("Monocraft", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(19, 345);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(303, 84);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "schwer";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.SlateBlue;
+            this.button8.Font = new System.Drawing.Font("Monocraft", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(19, 256);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(303, 84);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "mittel";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.SlateBlue;
+            this.button10.Font = new System.Drawing.Font("Monocraft", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(19, 166);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(303, 84);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "einfach";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Plack the Hanet", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(0, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 29);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "OMMEN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monocraft", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(249, 36);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "SCHWIERIGKEIT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DASF";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ math1/MATH/MATH/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MATH
+{
+    public partial class Form1 : Form
+    {
+        private const int NumberOfOperators = 2;
+        private const int MaxPointsDefault = 15;
+
+        private int answer = 0;
+        private int points = -1;
+        private int maxpoint = MaxPointsDefault;
+        private int range = 10;
+
+        private Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            roundstarter();
+        }
+
+        private void playSimpleSound(string soundname)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Path.Combine(Application.StartupPath, "content", soundname));
+            simpleSound.Play();
+        }
+
+        private void playSimpleSound2(string soundname)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Path.Combine(Application.StartupPath, "content", soundname));
+            simpleSound.Play();
+        }
+
+        public void roundstarter()
+        {
+            points++;
+            label3.Text = points.ToString();
+
+            int a = rnd.Next(range);
+            int b = rnd.Next(range);
+
+            int operato = rnd.Next(NumberOfOperators);
+
+            char operat = (operato == 0) ? '+' : '-';
+            label1.Text = $"{a} {operat} {b}";
+
+            answer = (operat == '+') ? a + b : a - b;
+
+            int changer1 = rnd.Next(10);
+            int changer2 = rnd.Next(10);
+            int changer3 = rnd.Next(10);
+
+            int[] answers = { answer, answer + changer1, answer - changer2, answer - changer3 };
+
+            for (int i = answers.Length - 1; i > 0; i--)
+            {
+                int j = rnd.Next(i + 1);
+                int temp = answers[i];
+                answers[i] = answers[j];
+                answers[j] = temp;
+            }
+
+            button1.Text = answers[0].ToString();
+            button2.Text = answers[1].ToString();
+            button3.Text = answers[2].ToString();
+            button4.Text = answers[3].ToString();
+        }
+
+        private void ButtonClickHandler(Button clickedButton)
+        {
+            if (int.TryParse(clickedButton.Text, out int clickedValue) && clickedValue == answer && maxpoint != Convert.ToInt16(label3.Text))
+            {
+                playSimpleSound("yes.wav");
+                roundstarter();
+            }
+            else if (Convert.ToInt16(label3.Text) == maxpoint)
+            {
+                MessageBox.Show("You won!");
+                this.Close();
+            }
+            else
+            {
+                points--;
+                playSimpleSound2("wrong.wav");
+                roundstarter();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonClickHandler((Button)sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ButtonClickHandler((Button)sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ButtonClickHandler((Button)sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ButtonClickHandler((Button)sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            maxpoint = 15;
+            roundstarter();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            maxpoint = 30;
+            roundstarter();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            maxpoint = 45;
+            roundstarter();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            range = 15;
+            panel1.Hide();
+            roundstarter();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            range = 30;
+            panel1.Hide();
+            roundstarter();
+        }
+    }
+}
